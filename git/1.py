657c3e69b4b53c7c20e83c3c139d18e97c60a07e
wqq <wqq@qq.com>
2026-06-13 16:08:26 +0800

    Add quick sort example in Python

--- git/1.py
+++ git/1.py
@@ -1,1 +1,17 @@
-111
+def quick_sort(arr):
+    """使用快速排序算法返回一个新的有序列表。"""
+    if len(arr) <= 1:
+        return arr
+
+    pivot = arr[len(arr) // 2]
+    left = [x for x in arr if x < pivot]
+    middle = [x for x in arr if x == pivot]
+    right = [x for x in arr if x > pivot]
+
+    return quick_sort(left) + middle + quick_sort(right)
+
+
+if __name__ == "__main__":
+    nums = [3, 6, 8, 10, 1, 2, 1]
+    sorted_nums = quick_sort(nums)
+    print(sorted_nums)
